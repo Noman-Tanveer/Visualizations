757cab21a5360e3e229ee9aae9e06e70302a970a
Noman Tanveer <nomantanveer021@gmail.com>
2020-12-08 20:56:11 +0500

    descriptions toooltips

--- api_call.py
+++ api_call.py
@@ -62,3 +62,34 @@ plot_dicts = [
 ]
 chart.add('', plot_dicts)
 chart.render_to_file('bar_descriptions.svg')
+
+names, plot_dicts = [], []
+for repo_dict in repo_dicts:
+    names.append(repo_dict['name'])
+    stars.append(repo_dict['stargazers_count'])
+
+    plot_dict = {
+        'value': repo_dict['stargazers_count'],
+        'label': repo_dict['description'],
+    }
+
+    plot_dicts.append(plot_dict)
+
+my_style = LS('#333399', Base_style = LCS)
+my_config = pygal.Config()
+my_config.x_label_rotation = 45
+my_config.show_legend = False
+my_config.title_font_size = 24
+my_config.label_font_size = 14
+my_config.major_label_font_size = 36
+my_config.truncate_label = 15
+my_config.show_y_guides = False
+my_config.width = 1000
+chart = pygal.Bar(my_config, style=my_style)
+
+# chart = pygal.Bar(style=my_style, x_label_rotation=45, show_legend=False)
+chart.title = 'Most-Starred Python Projects on GitHub'
+chart.x_labels = names
+
+chart.add('', plot_dict)
+chart.render_to_file('python_repos_description_tooltips.svg')
